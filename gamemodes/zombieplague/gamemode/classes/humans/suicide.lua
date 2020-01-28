c63edae419a78f657b146a1d4c2c9022367ffc65
nicholas.fuchs <nicksf13@gmail.com>
2020-01-28 12:37:50 -0300

    fix(ZP-2): Fixed team kill in suicide human

--- gamemodes/zombieplague/gamemode/classes/humans/suicide.lua
+++ gamemodes/zombieplague/gamemode/classes/humans/suicide.lua
@@ -2,21 +2,30 @@ ZPClass.Name = "HumanSuicideClassName"
 ZPClass.Description = "HumanSuicideClassDescription"
 ZPClass.MaxHealth = 50
 ZPClass.PModel = "models/player/guerilla.mdl"
-ZPClass.Speed = 230
-ZPClass.RunSpeed = 240
-ZPClass.CrouchSpeed = 0.4
+ZPClass.Speed = 180
+ZPClass.RunSpeed = 220
+ZPClass.CrouchSpeed = 0.5
 ZPClass.Breath = 50
 ZPClass.AbilityRecharge = 60
 function ZPClass:Ability(ply)
 	ply:Kill()
 	local explosion = ents.Create("env_explosion") -- https://facepunch.com/showthread.php?t=1021105 / Brandan
 	explosion:SetKeyValue("spawnflags", 144)
-	explosion:SetKeyValue("iMagnitude", 15)
+	explosion:SetKeyValue("iMagnitude", 3000)
 	explosion:SetKeyValue("iRadiusOverride", 256)
+	explosion:SetOwner(ply)
 	explosion:SetPos(ply:GetPos())
 	explosion:Spawn()
 	explosion:Activate()
 	explosion:Fire("explode","",0)
+
+	local TimerName = "Suicide" .. ply:SteamID64()
+	local EventName = "ZPResetAbilityEvent" .. ply:SteamID64()
+	hook.Add(EventName, TimerName, function()
+		ply:SetNextAbilityUse(0)
+		
+		hook.Remove(EventName, TimerName)
+	end)
 end
 
 if(ZPClass:ShouldBeEnabled()) then
