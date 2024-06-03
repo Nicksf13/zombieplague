33343ada39bd7d44eb6ce4547fe2ad1031d5f31d
Nicksf13 <nicksf13@gmail.com>
2024-06-03 16:51:14 -0300

    change(#316): Change zombie plague bank's folder

--- gamemodes/zombieplague/gamemode/bankstorage/text.lua
+++ gamemodes/zombieplague/gamemode/bankstorage/text.lua
@@ -1,14 +1,31 @@
 local BankStorageSource = {}
 function BankStorageSource:Init()
-    if !file.Exists("zombie_plague", "DATA") then
-        file.CreateDir("zombie_plague")
+    if !file.Exists("zombieplague", "DATA") then
+        file.CreateDir("zombieplague")
+    end
+    if !file.Exists("zombieplague/bank", "DATA") then
+        file.CreateDir("zombieplague/bank")
+    end
+
+    --Migrates old folder structure files
+    if file.Exists("zombie_plague", "DATA") then
+        local Files, Directories = file.Find("zombie_plague/*.txt", "DATA")
+
+        for k, File in pairs(Files) do
+            local AmmoPacks = file.Read("zombie_plague/" .. File, "DATA")
+            file.Write("zombieplague/bank/" .. File, AmmoPacks)
+
+            file.Delete("zombie_plague/" .. File, "DATA")
+        end
+
+        file.Delete("zombie_plague", "DATA")
     end
 end
 function BankStorageSource:GetPlayerAmmopacks(ply)
-    return tonumber(file.Read("zombie_plague/" .. ply:SteamID64() .. ".txt", "DATA") or "0")
+    return tonumber(file.Read("zombieplague/bank/" .. ply:SteamID64() .. ".txt", "DATA") or "0")
 end
 BankStorageSource.Save = function(SteamID64, Amount)
-    file.Write("zombie_plague/" .. SteamID64 .. ".txt", Amount)
+    file.Write("zombieplague/bank/" .. SteamID64 .. ".txt", Amount)
 end
 
 Bank.BankStorageSource = BankStorageSource
